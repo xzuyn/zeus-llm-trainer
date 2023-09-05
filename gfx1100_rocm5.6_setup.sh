44f4133c64d90a51fff40d7590726ecb2dd147fa
xzuyn <16216325+xzuyn@users.noreply.github.com>
2023-09-05 17:39:27 -0400

    Add files via upload

--- gfx1100_rocm5.6_setup.sh
+++ gfx1100_rocm5.6_setup.sh
@@ -20,41 +20,29 @@ fi
 # Verify that the virtual environment is activated
 python --version
 
-# Step 1/5: Installing Requirements
+# Step 1/4: Installing Requirements
 echo ""
 echo "Step 1/5: Installing Requirements"
 echo ""
-pip install -r requirements-rocm.txt
+pip install -r requirements.txt
 
-# Step 2/5: Removing Unwanted PyTorch and TorchVision Installations
+# Step 2/4: Removing Unwanted bitsandbytes Installation
 echo ""
-echo "Step 2/5: Removing Unwanted PyTorch and TorchVision Installations"
-echo ""
-pip uninstall -y torch torchvision
-
-# Step 3/5: Installing PyTorch and TorchVision for ROCm5.6
-echo ""
-echo "Step 3/5: Installing PyTorch and TorchVision for ROCm5.6"
-echo ""
-pip install --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/rocm5.6
-
-# Step 3/5: Removing Unwanted bitsandbytes Installation
-echo ""
-echo "Step 3/5: Removing Unwanted bitsandbytes Installation"
+echo "Step 2/5: Removing Unwanted bitsandbytes Installation"
 echo ""
 pip uninstall bitsandbytes
 
-# Step 4/5: Cloning the bitsandbytes-rocm Fork's Repo
+# Step 3/4: Cloning the bitsandbytes-rocm Fork's Repo
 echo ""
-echo "Step 4/5: Cloning the bitsandbytes-rocm Fork's Repo"
+echo "Step 3/5: Cloning the bitsandbytes-rocm Fork's Repo"
 echo ""
 cd venv
 git clone https://github.com/arlo-phoenix/bitsandbytes-rocm-5.6.git bitsandbytes
 cd bitsandbytes
 
-# Step 5/5: Installing bitsandbytes-rocm
+# Step 4/4: Installing bitsandbytes-rocm
 echo ""
-echo "Step 5/5: Installing bitsandbytes-rocm"
+echo "Step 4/4: Installing bitsandbytes-rocm"
 echo ""
 export ROCM_HOME=/opt/rocm-5.6.0
 make hip ROCM_TARGET=gfx1100
@@ -62,3 +50,4 @@ pip install .
 pip install scipy  # I think there's a requirements problem somewhere (at least in kohya_ss)
 cd ..
 cd ..
+
